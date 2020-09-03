0415dff05e67130744ca7e95c8d9e837784b9963
SatrikoAditya <satriko4d@gmail.com>
2020-09-03 22:09:08 +0700

    - add validate to user model - add router for login - add static method for login in UserController class

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router()
 const RegisterController = require("./registerRouter")
+const loginRouter = require('./loginRouter')
+
 
 router.use("/register", RegisterController)
+router.use("/login", loginRouter)
 
 module.exports = router
