2c37b49d42325e89cc7e5d6ec27e4c4bfd267921
SatrikoAditya <satriko4d@gmail.com>
2020-09-04 00:26:04 +0700

    - add authentication to access football fixtures

--- server/controllers/FootballController.js
+++ server/controllers/FootballController.js
@@ -3,13 +3,14 @@ const axios = require('axios')
 class FootballController {
     static async fixtures(req, res, next) {
         try {
+            const { from, to } = req.params
             const endpoint = 'https://allsportsapi.com/api/football'
             const response = await axios.get(endpoint, {
                 params: {
                     met: 'Fixtures',
                     APIkey: process.env.FOOTBALL_API_KEY,
-                    from: '2020-09-12',
-                    to: '2020-09-12'
+                    from: from || '2020-09-12',
+                    to: to || '2020-09-12'
                 }
             });
             let result = []
