46fc7eb340247eade0d1ef45b037101a42ba14a2
Rifkalagatha <mrifkalagatha@gmail.com>
2020-09-03 19:46:22 +0700

    first-init

---
+++ server/helpers/bcrypt.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcryptjs');
+
+function hashPassword(password) {
+    let salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+}
+
+function comparePassword(password, hashPass) {
+    return bcrypt.compareSync(password, hashPass)
+}
+
+module.exports = { hashPassword, comparePassword }
+
