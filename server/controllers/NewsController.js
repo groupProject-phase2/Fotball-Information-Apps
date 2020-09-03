e0cbc0ec09a3a6b1b21605be17e3d6c4283e43d9
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2020-09-03 23:36:31 +0700

    Added news

---
+++ server/controllers/NewsController.js
@@ -0,0 +1,26 @@
+const axios = require("axios")
+
+const API_KEY = process.env.NEWS_API_KEY
+const BASE_URL = "http://newsapi.org/v2/"
+
+class NewsController {
+  static async findAll(req, res, next) {
+    const { country, category } = req.query
+    try {
+      const response = await axios.get(`${BASE_URL}/top-headlines`, {
+        params: {
+          country: country || "id",
+          category: category || "sports",
+          apiKey: API_KEY,
+        },
+      })
+
+      res.status(200).json(response.data)
+    } catch (ex) {
+      // TODO ERROR HANDLING
+      res.json(ex)
+    }
+  }
+}
+
+module.exports = NewsController
